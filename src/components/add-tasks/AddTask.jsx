24e258e62aa10ae3c2ae373bf73bd903414b5843
Thiago Pereira da Silva <thi.pereira1704@gmal.com>
2022-02-04 22:27:48 -0300

    função add Task feita

--- src/components/add-tasks/AddTask.jsx
+++ src/components/add-tasks/AddTask.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState} from "react";
 
-import '../../styles/add-task/add-task.css'
+import "../../styles/add-task/add-task.css";
 
-export const AddTask = () => {
-    return (
-        <>
-            <div className="add-task-container">
-                <div className="input-container__add-task">
-                    <input type="text" name="add-task" id="add-task" placeholder='Digite uma tarefa'/>
-                </div>
-                <div className="button__add-task">
-                    <button>Adicionar</button>
-                </div>
-            </div>
-        </>
-    )
+export const AddTask = ({ handleTaskAdition }) => {
+  const [taskName, setTaskName] = useState("");
+
+  const handleInputChange = (event) => setTaskName(event.target.value);
+
+  const handleOnClick = () => handleTaskAdition(taskName);
+
+  return (
+    <>
+      <div className="add-task-container">
+        <div className="input-container__add-task">
+          <input
+            type="text"
+            name="add-task"
+            id="add-task"
+            placeholder="Digite uma tarefa"
+            onChange={handleInputChange}
+            value={taskName}
+          />
+        </div>
+        <div className="button__add-task">
+          <button onClick={handleOnClick}>Adicionar</button>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/pages/todo/Todo.jsx
+++ src/pages/todo/Todo.jsx
@@ -1,27 +1,22 @@
-import React, { useState } from 'react';
-import { AddTask } from '../../components/add-tasks/AddTask';
+import React, { useState } from "react";
+import { AddTask } from "../../components/add-tasks/AddTask";
 
-import { Tasks } from '../../components/tasks/Tasks';
+import { Tasks } from "../../components/tasks/Tasks";
 
-import '../../styles/todo/todo.css'
+import "../../styles/todo/todo.css";
 
 export const Todo = () => {
     const [data, setData] = useState([
-        { name: 'Ler' },
-        { name: 'Cozinhar' },
-        { name: 'Tocar' },
-        { name: 'Cantar' },
-        { name: 'Cantar' },
-        { name: 'Cantar' },
-        { name: 'Cantar' },
-        { name: 'Cantar' },
-        { name: 'Cantar' },
+        { id: Math.random(), name: "Ler", completed: false },
     ]);
+
+    const handleTaskAdition = (taskName) =>
+        setData([...data, { id: Math.random(), name: taskName, completed: false }]);
 
     return (
         <main className="todo-container">
-            <AddTask />
+            <AddTask handleTaskAdition={handleTaskAdition} />
             <Tasks data={data} />
         </main>
-    )
+    );
 };

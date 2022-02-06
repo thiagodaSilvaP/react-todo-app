9519578ade1921797fc52a2eabe017813c752480
Thiago Pereira da Silva <thi.pereira1704@gmal.com>
2022-02-06 12:22:17 -0300

    funções conclusao, deleção e edição de tarefa, feita. Falta o visual.

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { Task } from '../task/Task';
+import { Task } from "../task/Task";
 
-import '../../styles/tasks/tasks.css'
+import "../../styles/tasks/tasks.css";
 
-export const Tasks = ({ data }) => {
-    return (
-        <div className="tasks-container">
-            {data.map((task, index) => <Task task={task} key={index}/>)}
-        </div>
-    )
+export const Tasks = ({ data, handleTaskDeletion, handleTaskComplete, handleTaskEdit }) => {
+  return (
+    <div className="tasks-container">
+      {data.map((task, index) => (
+        <Task
+          task={task}
+          key={index}
+          handleTaskDeletion={handleTaskDeletion}
+          handleTaskComplete={handleTaskComplete}
+          handleTaskEdit={handleTaskEdit}
+        />
+      ))}
+    </div>
+  );
 };
--- src/pages/todo/Todo.jsx
+++ src/pages/todo/Todo.jsx
@@ -6,17 +6,44 @@ import { Tasks } from "../../components/tasks/Tasks";
 import "../../styles/todo/todo.css";
 
 export const Todo = () => {
-    const [data, setData] = useState([
-        { id: Math.random(), name: "Ler", completed: false },
-    ]);
+  const [data, setData] = useState([
+    { id: Math.random(), name: "Ler", completed: false },
+  ]);
 
-    const handleTaskAdition = (taskName) =>
-        setData([...data, { id: Math.random(), name: taskName, completed: false }]);
+  const handleTaskAdition = (taskName) =>
+    setData([...data, { id: Math.random(), name: taskName, completed: false }]);
 
-    return (
-        <main className="todo-container">
-            <AddTask handleTaskAdition={handleTaskAdition} />
-            <Tasks data={data} />
-        </main>
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = data.filter((task) => task.id !== taskId);
+    console.log(newTasks);
+    setData(newTasks);
+  };
+
+  const handleTaskComplete = (taskId) => {
+    const newTasks = data.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : { ...task }
     );
+    setData(newTasks);
+  };
+
+  const handleTaskEdit = (taskId, taskName) => {
+    const newTasks = data.map((task) =>
+      task.id === taskId ? { ...task, name: taskName } : { ...task }
+    );
+    setData(newTasks);
+  };
+
+  console.log(data);
+
+  return (
+    <main className="todo-container">
+      <AddTask handleTaskAdition={handleTaskAdition} />
+      <Tasks
+        data={data}
+        handleTaskDeletion={handleTaskDeletion}
+        handleTaskComplete={handleTaskComplete}
+        handleTaskEdit={handleTaskEdit}
+      />
+    </main>
+  );
 };
